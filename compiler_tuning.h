b845eaef19439e732c2be29a2750b35f64be75cf
Stepan Bylkov <scroogemcfawk@gmail.com>
2024-01-30 16:27:01 +0700

    init

---
+++ compiler_tuning.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifdef av_err2str
+#undef av_err2str
+#include <string>
+av_always_inline std::string av_err2string(int errnum) {
+    char str[AV_ERROR_MAX_STRING_SIZE];
+    return av_make_error_string(str, AV_ERROR_MAX_STRING_SIZE, errnum);
+}
+#define av_err2str(err) av_err2string(err).c_str()
+#endif
---
+++ notes.md
@@ -0,0 +1,5 @@
+Скинуть audio.opus файл
+
+Собрать пример
+
+Use 44.1 kHz sampling only
